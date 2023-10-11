3e30e9af7348454c63977448ed11767cbac8a6f1
Maciej Gomulec <maciej.gomulec@fdmgroup.com>
2023-10-11 21:08:39 +0200

    first commit

---
+++ src/main/java/com/karatesan/CashRestApi/CashRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.karatesan.CashRestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashRestApiApplication.class, args);
+	}
+
+}
